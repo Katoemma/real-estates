42777e2ab09610ad00478cfdf742dddc79bf90f5
Katoemma <103442484+Katoemma@users.noreply.github.com>
2023-03-09 16:38:24 +0300

    first  commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      'sm' : '376px' ,
+      'md' : '768px',
+      'lg' : '1024px',
+      'xl' : '1280'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
